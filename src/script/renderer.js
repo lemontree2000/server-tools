1e456cd26f911004a96c36fc742569413eca8bfc
lemontree <gonelove.guo@gmail.com>
2017-07-03 22:53:47 +0800

    完成加载页面时获取本机ip

--- src/script/renderer.js
+++ src/script/renderer.js
@@ -1,3 +1,21 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
+var omainIp = document.querySelector('.main .ip');
+
+function getIPAdress() {
+  var interfaces = require('os').networkInterfaces();
+  for (var devName in interfaces) {
+    var iface = interfaces[devName];
+    for (var i = 0; i < iface.length; i++) {
+      var alias = iface[i];
+      if (alias.family === 'IPv4' && alias.address !== '127.0.0.1' && !alias.internal) {
+        return alias.address;
+      }
+    }
+  }
+}  
+
+window.onload = function() {
+  omainIp.innerHTML = getIPAdress();
+};
